57a2e85f936cf2f2de980b1f40ebfca600fbeaf8
Robin Gayanger <robin.ga.fr@gmail.com>
2020-02-12 17:44:03 +0100

    Yelp MVP needs front

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      'Bordeaux',
+    phone_number:  '',
+    category:        "chinese"
+  },
+  {
+    name:         'Pizza East',
+    address:      'Bordeaux',
+    phone_number:  '',
+    category:        "italian"
+  },
+  {
+    name:         'McDo',
+    address:      'Bordeaux',
+    phone_number:  '',
+    category:        "japanese"
+  },
+  {
+    name:         'BK',
+    address:      'Bordeaux',
+    phone_number:  '',
+    category:        "french"
+  },
+  {
+    name:         'KFC',
+    address:      'Bordeaux',
+    phone_number:  '',
+    category:        "belgian"
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
